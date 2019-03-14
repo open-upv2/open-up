50c28a77838fe30a812db84a8e7c86322640a9e6
Na224248 <fafuang@gmail.com>
2019-03-14 21:16:52 +0700

    Finish Inventory

--- Rendering test open up!!!/Assets/script/PickUp.cs
+++ Rendering test open up!!!/Assets/script/PickUp.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PickUp : MonoBehaviour
 {
     public GameObject ObjectPick;
     public GameObject InvenObject;
+
+    static int InvenCount = 0;
 
     void OnMouseDown()
     {
         //if (require == "")
         //{
         ObjectPick.SetActive(false);
-        Debug.Log("pick helmet");
+        //Debug.Log("pick helmet");
         InvenObject.SetActive(true);
+        InvenCount++;
+        if(InvenCount >= 1)
+        {
+            InvenObject.transform.Translate((InvenCount-1) * 1, 0, 0);
+        }
+        Debug.Log(InvenCount);
         //}
         //check if have require item or not
     }
