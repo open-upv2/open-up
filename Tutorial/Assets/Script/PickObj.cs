23b641c8194c8242f38f7b3deca195eb178d23b5
Na224248 <fafuang@gmail.com>
2019-04-03 15:19:24 +0700

    New engine proof
    - Rewrite all game mechanic
       - Player movement
       - Pick up object method

---
+++ Tutorial/Assets/Script/PickObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickObj : MonoBehaviour
+{
+    public bool select = false;
+    public string Say = "Helmet";
+
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        select = (!select);
+    }
+}
---
+++ Tutorial/Assets/Script/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float speed = 4.0f;
+    Vector3 target;
+
+    private List<string> Inventory = new List<string>();
+
+    public bool stay = true;
+    public bool exit = true;
+    public float moveSpeed;
+
+    enum ObjectPick
+    {
+        Helmet = 0
+    }
+
+    string[] DialoguePick = new string[]
+    {
+        "Helmet",
+        "Hah??"
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.y = transform.position.y;
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<PickObj>().select)
+        {
+            //animation.Play("");
+            //yield WaitForSeconds(animation["die"].length);
+            Debug.Log(collision.gameObject.GetComponent<PickObj>().Say);
+            Inventory.Add("Weird Helmet");
+            Debug.Log(Inventory[0]);
+            Destroy(collision.gameObject);
+        }
+
+    }
+
+}
